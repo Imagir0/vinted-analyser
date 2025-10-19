1dc9e40d7d140cfc4d881c76edac79d395ad72e3
Imagiro <thomas.brun.perso@gmail.com>
2025-10-19 17:19:24 +0200

    openning window

--- src/main.js
+++ src/main.js
@@ -1,56 +1,30 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'node:path';
-import started from 'electron-squirrel-startup';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (started) {
-  app.quit();
-}
+const isDev = process.env.NODE_ENV === 'development';
+const VITE_DEV_SERVER_URL = 'http://localhost:5173'; // URL du serveur Vite
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+function createWindow() {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'), // facultatif
     },
   });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  if (isDev) {
+    // Mode développement : charger Vite dev server
+    win.loadURL(VITE_DEV_SERVER_URL);
+    win.webContents.openDevTools(); // Ouvrir DevTools si vous voulez
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    // Mode production : charger le fichier buildé
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
   }
+}
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+app.whenReady().then(createWindow);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,6 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// Ce fichier sert de pont sécurisé entre le front (index.html) et le back (main.js)
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Preload chargé');
+});
